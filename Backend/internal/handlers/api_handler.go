c9bd74bfd94387a7465d8bc8985354432c9f71a4
ns0851 <ns0851@srmist.edu.in>
2025-08-21 17:57:44 +0530

    working gemini and pexels API again

--- Backend/internal/handlers/api_handler.go
+++ Backend/internal/handlers/api_handler.go
@@ -1,11 +1,11 @@
 package handlers
 
 import (
-	"Backend/internal/api"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"Backend/internal/api"
 )
 
 func GeminiHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,8 +24,7 @@ func PexelsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal("Error getting response from Pexels: ", err)
 	}
-	// io.WriteString(w, `<h1>%s</h1><img src="%s" alt="photo">`, response)
+
 	fmt.Fprintf(w, `<h1 >Image</h1><img style="width:35vw; height:50vh;" src="%s" alt="photo">`, response)
-	// `<h1>%s</h1><img src="%s" alt="photo">`, response
 	fmt.Println("got /pexels request")
 }
--- Backend/internal/router/api_router.go
+++ Backend/internal/router/api_router.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 )
 
-func SetupRouter() *http.ServeMux {
+func APIRouter() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handlers.GetRoot)
+	mux.HandleFunc("/gemini", handlers.GeminiHandler)
+	mux.HandleFunc("/pexels", handlers.PexelsHandler)
 	return mux
 }
